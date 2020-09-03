acd79e32170ae604ed4f04b2ec847271e1cea1ff
kimototakaya <independensdey239@gmail.com>
2020-09-03 20:48:40 +0900

    controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,22 @@
 class UsersController < ApplicationController
   
   def new
+    binding.pry
     @user = User.new
   end
 
   def create
+    binding.pry
     @user = User.new(user_params)
     if @user.save
       redirect_to root_path(user.id), notice:'登録が完了しました'
     else
       render 'devise/registrations/new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "comments#index"
-  resources :user, only: [:index, :new, :create]
+  resources :users, only: [:index, :new, :create]
   resources :posts, only: [:index, :new, :create, :show] do
     resources :comments, only: [:index, :new, :create]
   end
