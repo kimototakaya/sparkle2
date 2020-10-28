20eacd8d9190d7c612d7755fc023ec316be5d369
kimototakaya <independensdey239@gmail.com>
2020-10-28 22:58:56 +0900

    post truncate

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # def truncate_description(description)
+  #   description = strip_tag(description).truncate(30)
+  # end
 end
