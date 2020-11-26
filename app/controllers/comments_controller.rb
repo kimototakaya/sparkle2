732740414249e4a4934bd99881db6f45281e3675
kimototakaya <independensdey239@gmail.com>
2020-11-26 23:51:28 +0900

    post show fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,7 @@ class CommentsController < ApplicationController
   end
   
   private
-
   def comment_params
-    # params.require(:comment).permit(:message).merge(user_id: current_user.id post_id: params[:post_id])
+    params.require(:comment).permit(:message).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
